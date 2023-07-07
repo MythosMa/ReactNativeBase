7ce59e460a81bf64fe21d4a89bebffbcbb9c81bc
MaYuan <mythosmayuan@gmail.com>
2023-07-07 11:48:26 +0800

    change project name

--- ios/reactnativebaseTests/reactnativebaseTests.m
+++ ios/reactnativebaseTests/reactnativebaseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface WZZDMobileTests : XCTestCase
+@interface reactnativebaseTests : XCTestCase
 
 @end
 
-@implementation WZZDMobileTests
+@implementation reactnativebaseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
